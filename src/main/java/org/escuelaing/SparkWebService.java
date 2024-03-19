3a90152d5eb0f80ae46cfca66229a48bb970530f
JordyBautista10 <jordy.sbautista@gmail.com>
2024-03-19 00:20:02 -0500

    Connection complete

--- src/main/java/org/escuelaing/SparkWebService.java
+++ src/main/java/org/escuelaing/SparkWebService.java
@@ -6,19 +6,21 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
-import static spark.Spark.post;
-import static spark.Spark.staticFileLocation;
+
+import static spark.Spark.*;
 
 public class SparkWebService {
 
     private static final String[] datos = new String[]{"35001", "35002", "35003"};
+    private static final String[] servers = new String[]{"logservice1", "logservice2", "logservice3"};
     private static int currentIndex = 0;
 
     public static void main(String[] args) {
+        port(getPort());
 
         staticFileLocation( "public");
 
-        post("log", (req,res) -> {
+        get("logs", (req,res) -> {
             String message = req.queryParams("msg");
             res.type("application/json");
             return makeRequest(roundRobin(message));
@@ -28,10 +30,12 @@ public class SparkWebService {
     public static String roundRobin(String message) {
         currentIndex = (currentIndex + 1) % datos.length;
         System.out.println( "Servidor que antiende: "  + datos[currentIndex]);
-        return "http://localhost:" + datos[currentIndex] + "/logs?msg=" + message;
+        return "http://" +  servers[currentIndex] + ":" + datos[currentIndex] + "/logs?msg=" + message;
     }
 
     public static String makeRequest(String url) throws IOException {
+
+        System.out.println("--------------------------------" + url);
 
         URI uri = URI.create(url);
         URL obj = uri.toURL();
@@ -42,12 +46,7 @@ public class SparkWebService {
         int responseCode = con.getResponseCode();
         StringBuilder response = new StringBuilder();
         // Encabezado necesario en todas las peticiones
-        response.append("""
-                HTTP/1.1 200 OK\r
-                Content-Type:application/json\r
-                Access-Control-Allow-Origin: *\r
-                \r
-                """);
+
         System.out.println("GET Response Code :: " + responseCode);
 
         if (responseCode == HttpURLConnection.HTTP_OK) { // success
@@ -68,5 +67,12 @@ public class SparkWebService {
         System.out.println("GET DONE");
 
         return response.toString();
+    }
+
+    public static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 8080;
     }
 }
